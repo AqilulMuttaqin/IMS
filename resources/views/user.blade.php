ff92933033ae697722491dae457f6c7ed60327fe
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-02-20 14:53:16 +0700

    card berhasil login

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Ini User</h1>
+<div class="container-xxl flex-grow-1 container-p-y">
+    <div class="card">
+        <div class="d-flex align-items-end row">
+            <div class="col-sm-7">
+                <div class="card-body">
+                    <h5 class="card-title text-primary">Congratulations</h5>
+                    <p class="mb-4">
+                        You have done login as a USER
+                    </p>
+
+                    <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                </div>
+            </div>
+            <div class="col-sm-5 text-center text-sm-left">
+                <div class="card-body pb-0 px-0 px-md-4">
+                    <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
+                        alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                        data-app-light-img="illustrations/man-with-laptop-light.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
