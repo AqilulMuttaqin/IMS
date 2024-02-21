c50aa3900badcdb2fc18539c1f1302703d5ca9bd
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2024-02-21 09:02:10 +0700

    export pdf

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Dompdf\Dompdf;
 use Illuminate\Http\Request;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use TCPDF;
 use ZipArchive;
 
 class QrCodeController extends Controller
@@ -64,5 +66,48 @@ class QrCodeController extends Controller
         $zip->close();
 
         return response()->download($zipFile, 'qr-codes.zip')->deleteFileAfterSend(true);
+    }
+
+    public function generatePdfWithQrCodes()
+    {
+        $dompdf = new Dompdf();
+        
+        $html = '<html><body>';
+
+        for ($i = 1; $i <= 35; $i++) {
+            if (($i - 1) % 20 === 0) {
+                if ($i !== 1) {
+                    $html .= '</div><div style="page-break-after: always;"></div>';
+                }
+                $html .= '<div style="display: flex; flex-wrap: wrap;">';
+            }
+
+            $html .= "<div style='width: 20%; padding: 10px;'>";
+            $qrCodeData = QrCode::size(150)
+                ->format('png')
+                ->generate("QR Code $i");
+
+            $qrCodeBase64 = base64_encode($qrCodeData);
+
+            $html .= "<img src='data:image/png;base64,{$qrCodeBase64}' style='width: 100%; height: auto;' />";
+            
+            $html .= "<p style='text-align: center;'>Name $i</p>";
+
+            $html .= '</div>';
+        }
+
+        $html .= '</div></body></html>';
+
+        // Load HTML content into DomPDF
+        $dompdf->loadHtml($html);
+
+        // Set paper size and orientation
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render PDF (Optional: You can save the PDF to a file by providing a file path instead of outputting it)
+        $dompdf->render();
+
+        // Output PDF
+        return $dompdf->stream('qrcodes.pdf');
     }
 }
