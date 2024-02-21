74af54e9b8bb892d64fa52edfd6292dd5de7be83
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-02-21 10:50:46 +0700

    padding datatable

--- resources/views/staff/data-barang.blade.php
+++ resources/views/staff/data-barang.blade.php
@@ -4,7 +4,7 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="card">
             <h5 class="card-header">Data Barang</h5>
-            <div class="table-responsive text-nowrap p-3">
+            <div class="table-responsive text-nowrap pt-0 p-3">
                 <table class="table table-striped" id="dataBarang">
                     <thead>
                         <tr>
