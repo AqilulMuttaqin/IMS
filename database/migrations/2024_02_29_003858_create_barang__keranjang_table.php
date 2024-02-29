49d4c51f30c5982164122b146370e48444d46e55
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2024-02-29 07:39:43 +0700

    Create Table Barang_keranjang

---
+++ database/migrations/2024_02_29_003858_create_barang__keranjang_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang_keranjang', function (Blueprint $table) {
+            $table->foreignId('pesanan_id')->constrained('pesanan')->onDelete('cascade');
+            $table->string('kode_js');
+            $table->integer('qty');
+            $table->timestamps();
+            $table->foreign('kode_js')->references('kode_js')->on('barang');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_keranjang');
+    }
+};
