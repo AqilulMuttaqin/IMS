ab93d6961aa7e61c8cd06908d1554b995cd40d31
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-01 09:50:14 +0700

    tambah warna pada status

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -9,21 +9,24 @@
                     <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab" data-bs-target="#menu-1"
                         aria-controls="menu-1" aria-selected="true">
                         <i class="tf-icons bx bx-message-alt-detail"></i> Pesanan Masuk
-                        <span id="pendingNotif" class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-danger">0</span>
+                        <span id="pendingNotif"
+                            class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-success">0</span>
                     </button>
                 </li>
                 <li class="nav-item">
                     <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#menu-2"
                         aria-controls="menu-2" aria-selected="false">
                         <i class="tf-icons bx bx-store-alt"></i> Perlu Disiapkan
-                        <span id="disiapkanNotif" class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-warning">0</span>
+                        <span id="disiapkanNotif"
+                            class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-warning">0</span>
                     </button>
                 </li>
                 <li class="nav-item">
                     <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#menu-3"
                         aria-controls="menu-3" aria-selected="false">
                         <i class="tf-icons bx bx-send"></i> Perlu Dikirim
-                        <span id="dikirimNotif" class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-success">0</span>
+                        <span id="dikirimNotif"
+                            class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-primary">0</span>
                     </button>
                 </li>
             </ul>
@@ -366,7 +369,7 @@
             get_data('pesananPerluDisiapkan', 'disiapkan');
 
             get_data('pesananPerluDikirim', 'dikirim');
-            
+
             //setInterval(updateNotif, 60000);
 
             setInterval(function() {
@@ -380,7 +383,9 @@
             $.ajax({
                 url: "{{ url()->current() }}",
                 method: 'GET',
-                data: { status: status }, // Pass the status parameter in the request
+                data: {
+                    status: status
+                }, // Pass the status parameter in the request
                 success: function(response) {
                     $('#pendingNotif').text(response.counts.pending);
                     $('#disiapkanNotif').text(response.counts.disiapkan);
@@ -388,27 +393,41 @@
                     var container = $('#' + containerId);
                     container.empty();
 
-                    $.each(response.pesanan, function(index, pesanan) { // Access the 'pesanan' property in the response
+                    $.each(response.pesanan, function(index,
+                        pesanan) { // Access the 'pesanan' property in the response
                         var card = $('<div class="col-sm-6 col-md-4 col-lg-3 my-2"></div>');
                         var cardBody = $('<div class="card border"></div>');
                         var cardHeader = $('<div class="card-header pb-1"></div>');
-                        var cardTitle = $('<h6 class="text-center">Pesanan Line ' + pesanan.id + '</h6>');
+                        var cardTitle = $('<h6 class="text-center">Pesanan Line ' + pesanan.id +
+                            '</h6>');
                         var hr = $('<hr>');
                         var cardCardBody = $('<div class="card-body"></div>');
 
                         var row = $('<div class="row"></div>');
-                        var nameLabelCol = $('<div class="col-6"><label for="label">Tanggal</label></div>');
-                        var nameValueCol = $('<div class="col-6"><p id="label">: ' + pesanan.user.name + '</p></div>');
-                        var detailLabelCol = $('<div class="col-6"><label for="detail">Detail</label></div>');
+                        var nameLabelCol = $(
+                            '<div class="col-6"><label for="label">Tanggal</label></div>');
+                        var nameValueCol = $('<div class="col-6"><p id="label">: ' + pesanan.user.name +
+                            '</p></div>');
+                        var detailLabelCol = $(
+                            '<div class="col-6"><label for="detail">Detail</label></div>');
                         var detailValueCol = $(
                             '<div class="col-6"><p id="detail">: <button type="button" class="btn btn-sm btn-primary" id="btnDetail" data-pesanan-id="' +
                             pesanan.id +
                             '" data-bs-toggle="modal" data-bs-target="#detailKonfirmasiModal"><i class="bx bx-show"></i></button></p></div>'
                         );
                         var hr = $('<hr>');
-                        var confirmButtonCol = $(
-                            '<div class="col-12"><button type="button" class="btn btn-warning w-100">Konfirmasi</button></div>'
-                        );
+                        var confirmButtonCol = $('<div class="col-12"></div>');
+                        var confirmButton = $('<button type="button" class="btn w-100"></button>');
+                        if (containerId === 'pesananMasuk') {
+                            confirmButton.text('Konfirmasi');
+                            confirmButton.addClass('btn-success');
+                        } else if (containerId === 'pesananPerluDisiapkan') {
+                            confirmButton.text('Kirim');
+                            confirmButton.addClass('btn-warning');
+                        } else if (containerId === 'pesananPerluDikirim') {
+                            confirmButton.text('Selesai');
+                            confirmButton.addClass('btn-primary');
+                        }
 
                         cardHeader.append(cardTitle);
                         cardHeader.append(hr);
@@ -418,6 +437,7 @@
                         row.append(detailLabelCol);
                         row.append(detailValueCol);
                         row.append(hr.clone());
+                        confirmButtonCol.append(confirmButton);
                         row.append(confirmButtonCol);
                         cardCardBody.append(row);
 
@@ -427,8 +447,6 @@
                         card.append(cardBody);
 
                         container.append(card);
-
-                        
                     });
                 }
             });
--- resources/views/staff/pesanan.blade.php
+++ resources/views/staff/pesanan.blade.php
@@ -17,79 +17,104 @@
                     </thead>
                     <tbody>
                         <!-- <tr>
-                            <td class="text-center">1</td>
-                            <td>Albert Cook</td>
-                            <td>
-                                <button class="btn btn-sm btn-primary d-flex align-items-center">
-                                    <i class="bx bx-show-alt me-1"></i>
-                                    Lihat Pesanan
-                                </button>
-                            </td>
-                            <td><span class="badge bg-label-info me-1">Request</span></td>
-                        </tr>
-                        <tr>
-                            <td class="text-center">2</td>
-                            <td>Jhon Dee</td>
-                            <td>
-                                <button class="btn btn-sm btn-primary d-flex align-items-center">
-                                    <i class="bx bx-show-alt me-1"></i>
-                                    Lihat Pesanan
-                                </button>
-                            </td>
-                            <td><span class="badge bg-label-warning me-1">Pending</span></td>
-                        </tr> -->
+                                <td class="text-center">1</td>
+                                <td>Albert Cook</td>
+                                <td>
+                                    <button class="btn btn-sm btn-primary d-flex align-items-center">
+                                        <i class="bx bx-show-alt me-1"></i>
+                                        Lihat Pesanan
+                                    </button>
+                                </td>
+                                <td><span class="badge bg-label-info me-1">Request</span></td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">2</td>
+                                <td>Jhon Dee</td>
+                                <td>
+                                    <button class="btn btn-sm btn-primary d-flex align-items-center">
+                                        <i class="bx bx-show-alt me-1"></i>
+                                        Lihat Pesanan
+                                    </button>
+                                </td>
+                                <td><span class="badge bg-label-warning me-1">Pending</span></td>
+                            </tr> -->
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-    
-    @endsection
-    
-    @push('scripts')
-        <script>
-            $(document).ready(function () {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                var table = $('#dataPesanan').DataTable({
-                    processing: true,
-                    serverSide: true,
-                    ajax: {
-                        url: '{{ url()->current() }}',
-                        type: 'GET'
-                    },
-                    columns: [
-                        { data: 'DT_RowIndex', name: 'DT_RowIndex' },
-                        { data: 'user.name', name: 'user.name' },
-                        { 
-                            data: function(row) {
-                                return row.barang.map(function(item) {
-                                    return item.nama;
-                                }).join('<br>');
-                            },
-                            name: 'barang.nama'
-                        },
-                        { 
-                            data: function(row) {
-                                return row.barang.map(function(item) {
-                                    return item.pivot.qty;
-                                }).join('<br>');
-                            },
-                            name: 'barang.pivot.qty'
-                        },
-                        { data: 'status', 
-                            name: 'status', 
-                            render: function (data, type, full, meta) {
-                                return `
-                                <td><span class="badge bg-label-warning me-1">`+ data + `</span></td>
-                                `;
-                            }
-                        },
-                    ]
-                });
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
             });
-        </script>
-    @endpush
+            var table = $('#dataPesanan').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ url()->current() }}',
+                    type: 'GET'
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    },
+                    {
+                        data: 'user.name',
+                        name: 'user.name'
+                    },
+                    {
+                        data: function(row) {
+                            return row.barang.map(function(item) {
+                                return item.nama;
+                            }).join('<br>');
+                        },
+                        name: 'barang.nama'
+                    },
+                    {
+                        data: function(row) {
+                            return row.barang.map(function(item) {
+                                return item.pivot.qty;
+                            }).join('<br>');
+                        },
+                        name: 'barang.pivot.qty'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status',
+                        render: function(data, type, full, meta) {
+                            var statusClass;
+                            switch (data) {
+                                case "pending":
+                                    statusClass = "bg-label-danger";
+                                    break;
+                                case "disiapkan":
+                                    statusClass = "bg-label-warning";
+                                    break;
+                                case "dikirim":
+                                    statusClass = "bg-label-info";
+                                    break;
+                                case "terkirim":
+                                    statusClass = "bg-label-primary";
+                                    break;
+                                case "selesai":
+                                    statusClass = "bg-label-info";
+                                    break;
+                                default:
+                                    statusClass = "";
+                            }
+                            return `
+                                <td><span class="badge ${statusClass} me-1">`+ data + `</span></td>
+                            `;
+                        }
+                    },
+                ]
+            });
+        });
+    </script>
+@endpush
