a03d11c2e32833afba52a95704269554b6014397
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-04 13:18:33 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,59 @@
-<!DOCTYPE html>
-<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default">
+<!doctype html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>Login Production Inventory - SAI</title>
-    <meta name="description" content="" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/logo.png') }}" />
-    <link href="{{ asset('assets/css/css2.css') }}" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
-    <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
-    <script src="{{ asset('assets/js/config.js') }}"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}" />
+    <link rel="stylesheet" href="{{ asset('src/assets/css/styles.min.css') }}" />
 </head>
 
 <body>
-    <div class="container-xxl">
-        <div class="authentication-wrapper authentication-basic container-p-y">
-            <div class="authentication-inner">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="app-brand justify-content-center mb-2">
-                            <div class="app-brand-link gap-2">
-                                <span class="app-brand-logo demo">
-                                    <img src="{{ asset('assets/img/logo.png') }}" alt="" width="30">
-                                </span>
-                                <span class="app-brand-text demo text-body fw-bolder" style="text-transform: uppercase;">ims</span>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div
+            class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-8 col-lg-6 col-xxl-3">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                    <img src="{{ asset('images/pims.png') }}" width="180" alt="">
+                                </a>
+                                <p class="text-center">Production Inventory Management System</p>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="nik" class="form-label">Nik</label>
+                                        <input type="text" class="form-control" id="nik" name="nik"
+                                            placeholder="Enter your Nik" autocomplete="off" autofocus required />
+                                    </div>
+                                    <div class="mb-5 form-password-toggle">
+                                        <div class="d-flex justify-content-between">
+                                            <label class="form-label" for="password">Password</label>
+                                        </div>
+                                        <div class="input-group input-group-merge">
+                                            <input type="password" id="password" class="form-control" name="password"
+                                                placeholder="Enter your password" autocomplete="off"
+                                                aria-describedby="password" required />
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <button type="submit"
+                                        class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</button>
+                                </form>
                             </div>
                         </div>
-                        <h6 class="mb-2 text-center">Welcome to Production Inventory 👋</h6>
-                        <hr>
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="nik" class="form-label">Nik</label>
-                                <input type="text" class="form-control" id="nik" name="nik" placeholder="Enter your Nik" autocomplete="off" autofocus required />
-                            </div>
-                            <div class="mb-3 form-password-toggle">
-                                <div class="d-flex justify-content-between">
-                                    <label class="form-label" for="password">Password</label>
-                                </div>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password" autocomplete="off" aria-describedby="password" required />
-                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="mb-3">
-                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
-    <script src="{{ asset('assets/js/main.js') }}"></script>
-    <script async defer src="{{ asset('assets/js/buttons.js') }}"></script>
+    <script src="{{ asset('src/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
