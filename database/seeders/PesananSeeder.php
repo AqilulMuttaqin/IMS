46fec5696181d01db389e501a29227cd441364b9
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-06 11:13:17 +0700

    database

--- database/seeders/PesananSeeder.php
+++ database/seeders/PesananSeeder.php
@@ -11,22 +11,22 @@ class PesananSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    // public function run(): void
-    // {
-    //     $status = ['pending', 'disiapkan', 'dikirim'];
-    //     for ($i = 0; $i < 10; $i++) {
-    //         $pesanan = Pesanan::create([
-    //           'user_id' => 3,
-    //             'status' => $status[rand(0, 2)],
-    //         ]);
+    public function run(): void
+    {
+        $status = ['pending', 'disiapkan', 'dikirim'];
+        for ($i = 0; $i < 0; $i++) {
+            $pesanan = Pesanan::create([
+              'user_id' => 3,
+                'status' => $status[rand(0, 2)],
+            ]);
           
-    //         for ($j = 0; $j < rand(1, 5); $j++) {
-    //           $barangId = 'A00' . sprintf('%02d', rand(1, 20));
-    //           $qty = rand(1, 10);
+            for ($j = 0; $j < rand(1, 5); $j++) {
+              $barangId = 'A00' . sprintf('%02d', rand(1, 20));
+              $qty = rand(1, 10);
           
-    //           $pesanan->barang()->attach($barangId, ['qty' => $qty]);
-    //         }
-    //       }
+              $pesanan->barang()->attach($barangId, ['qty' => $qty]);
+            }
+          }
 
-    // }
+    }
 }
