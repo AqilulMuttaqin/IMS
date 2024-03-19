fe2b87ffcf9a9f706e96533ec222d6f3e5a3f119
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-19 11:15:59 +0700

    dashboard

--- public/src/assets/js/dashboard.js
+++ public/src/assets/js/dashboard.js
@@ -2,15 +2,53 @@ $(function () {
 
   var chart = {
     chart: {
-      type: 'line',
-      height: 280
+      type: "area",
+      height: 230
     },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
+    colors: ["#00BAEC"],
+    stroke: {
+      width: 3,
+      curve: "straight"
+    },
+    dataLabels: {
+      enabled: false
+
+    },
+    fill: {
+      gradient: {
+        enabled: true,
+        opacityFrom: 0.55,
+        opacityTo: 0
+      }
+    },
+    markers: {
+      size: 5,
+      colors: ["#fff"],
+      strokeColor: "#00BAEC",
+      strokeWidth: 3
+    },
+    series: [
+      {
+        name: "Series 1",
+        data: [45, 52, 38, 45, 19, 23, 2, 30, 29, 28, 29, 19, 41]
+      }
+    ],
     xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+      categories: [
+        "01 Jan",
+        "02 Jan",
+        "03 Jan",
+        "04 Jan",
+        "05 Jan",
+        "06 Jan",
+        "07 Jan",
+        "08 Jan",
+        "09 Jan",
+        "10 Jan",
+        "11 Jan",
+        "12 Jan",
+        "13 Jan",
+      ],
     }
   };
 
--- resources/views/spv/dashboard.blade.php
+++ resources/views/spv/dashboard.blade.php
@@ -56,4 +56,38 @@
             <div id="chart" style="height: 40px"></div>
         </div>
     </div>
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h5>Detail History Barang</h5>
+                </div>
+                <div class="col-sm-7">
+                    <div class="d-flex justify-content-end text-end">
+                        <button type="button" class="btn btn-sm btn-success d-flex align-items-center" id="exportBtn">
+                            <i class="ti ti-file-export me-1"></i>
+                            Export Excel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive text-nowrap">
+                <table class="table w-100" id="dataDetailBarang">
+                    <thead>
+                        <tr>
+                            <th style="width: 20px">No</th>
+                            <th>Kode JS</th>
+                            <th>Nama</th>
+                            <th>In</th>
+                            <th>Out</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
