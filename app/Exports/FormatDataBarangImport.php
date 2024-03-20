b5d4d6740bd61de867b32743c9f5445eb15b2120
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2024-03-20 13:37:14 +0700

    Export Format import

---
+++ app/Exports/FormatDataBarangImport.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\DataBarang;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class FormatDataBarangImport implements FromCollection, WithTitle, WithStyles, WithEvents
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return collect([
+            'Kode JS',
+            'Invoice Number',
+            'PO Number',
+            'Qty',
+            'Lokasi'
+        ]);
+    }
+
+    public function title(): string
+    {
+        return "Format Data Barang Import";
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $styleArray = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ];
+
+        $sheet->getStyle('A1:E1')->applyFromArray($styleArray);
+        $sheet->getStyle('A2:E' . ($sheet->getHighestRow()))->applyFromArray($styleArray);
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $cellRange = 'A1:E1';
+
+                $event->sheet->getStyle($cellRange)->applyFromArray([
+                    'font' => ['bold' => true],
+                    'fill' => ['fillType' => Fill::FILL_SOLID, 'startColor' => ['rgb' => '9ef0e6']],
+                ]);
+                
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setBold(true);
+
+                foreach (range('A','E') as $column) {
+                    $event->sheet->getDelegate()->getColumnDimension($column)->setAutoSize(true);
+                }
+            },
+        ];
+    }
+}
