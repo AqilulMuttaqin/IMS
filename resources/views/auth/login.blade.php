591653cca1faf2a219a5efa20990f55ae3c485d7
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-22 14:26:14 +0700

    update view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                                         <input type="text" class="form-control" id="nik" name="nik" placeholder="Enter your Nik" autocomplete="off" required />
                                         <label for="nik" class="form-label">Nik</label>
                                     </div>
-                                    <div class=" form-floating mb-5">
+                                    <div class=" form-floating mb-4">
                                         <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password" autocomplete="off" aria-describedby="password" required />
                                         <label class="form-label" for="password">Password</label>
                                     </div>
