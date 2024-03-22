9931a867f3021b4a4ce0716604f787ae129e523f
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2024-03-22 07:42:42 +0700

    initiate STO

---
+++ app/Http/Controllers/STOController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\STO;
+use App\Http\Requests\StoreSTORequest;
+use App\Http\Requests\UpdateSTORequest;
+
+class STOController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSTORequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(STO $sTO)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(STO $sTO)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSTORequest $request, STO $sTO)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(STO $sTO)
+    {
+        //
+    }
+}
