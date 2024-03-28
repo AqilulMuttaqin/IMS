158fcee7a0dfb9f7a9856004dcf209bf7ab33a59
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-03-28 14:08:19 +0700

    sdasds

--- resources/views/user/home-user.blade.php
+++ resources/views/user/home-user.blade.php
@@ -118,20 +118,19 @@
         </div>
     </div>
 
-
-<div class="toast-container position-fixed bottom-0 end-0 p-3">
-  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-      <img src="..." class="rounded me-2" alt="...">
-      <strong class="me-auto">Shin-PSC</strong>
-      <small>now</small>
-      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <img src="{{ asset('images/check.png ')}}" class="rounded me-2" alt="..." style="width: 20px">
+                <strong class="me-auto">Success</strong>
+                <small class="">now</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+            <div class="toast-body bg-light">
+                Hello, world! This is a toast message.
+            </div>
+        </div>
     </div>
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-  </div>
-</div>
 
     <!-- JavaScript -->
     <script>
